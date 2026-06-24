704a79b2a843fc2ec1999ddf58e3fc78a072de35
Tsiory <tsitsinoms@gmail.com>
2026-06-24 17:36:11 +0300

    hommeee

--- back/app.py
+++ back/app.py
@@ -16,8 +16,7 @@ position_history = []
 
 @app.route("/")
 def home():
-    return 
-render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/play", methods=["POST"])
 def play():
